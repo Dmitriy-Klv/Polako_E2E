98a25f7bea7795b82bd9029c3de1d72c9a1ae98a
Dmitriy-Klv <dmklvd@gmail.com>
2026-05-20 15:06:33 +0200

    implement BaseTest with autouse fixture and update tests style

--- TP_Polako_E2E/tests/test_events.py
+++ TP_Polako_E2E/tests/test_events.py
@@ -1,17 +1,13 @@
-from TP_Polako_E2E.pages.auth.login_page import LoginPage
-from TP_Polako_E2E.pages.events.events_list_page import EventsListPage
-from TP_Polako_E2E.pages.profile.user_profile_page import UserProfilePage
+from TP_Polako_E2E.base.base_test import BaseTest
 
 
+class TestEvent(BaseTest):
 
-def test_create_event_button_is_clickable(app_page):
-    login = LoginPage(app_page)
-    user_profile = UserProfilePage(app_page)
-    events_list = EventsListPage(app_page)
+    def test_create_event_button_is_clickable(self):
+        self.login_page.login_as_valid_user()
+        self.login_page.click_profile()
 
-    login.login_as_valid_user()
-    login.click_profile()
-    user_profile.click_event_management_link()
+        self.user_profile.click_event_management_link()
 
-    events_list.create_event_btn_is_visible()
-    events_list.click_create_event_btn()
+        self.events_list.create_event_btn_is_visible()
+        self.events_list.click_create_event_btn()

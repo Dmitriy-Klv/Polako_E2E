186ea2e3071ede552b430c0afe0b21594b2880b1
devsmish <smyshynskyi@gmail.com>
2026-05-25 22:29:02 +0200

    add tests for the simple user profile and fixed header tests

--- TP_Polako_E2E/utils/constants.py
+++ TP_Polako_E2E/utils/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-sections_mapping = {
+SECTIONS_MAPPING = {
     "events": "#events",
     "prices": "pricing",
     "tickets": "https://polako-tickets.rs/index-ru.html",
@@ -11,7 +11,7 @@ sections_mapping = {
 }
 
 
-expected_markers = {
+EXPECTED_MARKERS = {
     "telegram": "t.me/polakohedonist",
     "instagram_ru": "instagram.com/polakohedonist/",
     "instagram_sr": "instagram.com/polakohedonist.dogadjaji",
@@ -19,6 +19,7 @@ expected_markers = {
     "viber": "viber://chat",
     "whatsapp": "whatsapp.com",
 }
+
 # LoginPage
 TEST_EMAIL = "test@mail.com"
 INVALID_PASSWORD = "WrongPassword123"
@@ -46,3 +47,36 @@ EVENT_TO_DELETE = EVENT_NAME
 ROOT_DIR = Path(__file__).resolve().parent.parent
 IMAGE_PATH = ROOT_DIR / "test_data" / "test_events_foto.png"
 TITLE_TEXT_RESULT = "The event title is empty."
+
+VALID_PROFILE_DATA = {
+    "first_name": "Ramses",
+    "last_name": "Fourth",
+    "email": "sergioodessit+1@gmail.com",
+    "phone": "+1234567890",
+    "instagram": "@ramsey",
+    "telegram": "@ram4"
+}
+
+PARTIAL_PROFILE_DATA = {
+    "first_name": "Привет",
+    "last_name": "",
+    "email": "hello@icloud.com",
+    "phone": "",
+    "instagram": "",
+    "telegram": "@hello"
+}
+
+INVALID_PROFILE_DATA = {
+    "first_name": "932c- mv3c kmf in0 \[w [wld][mcna]чьэцуст0ш3ьц0ч3 932c- mv3c kmf in0 \[w [wld][mcna]чьэцуст0ш3ьц0ч3",
+    "last_name": "932c- mv3c kmf in0 \[w [wld][mcna]чьэцуст0ш3ьц0ч3 932c- mv3c kmf in0 \[w [wld][mcna]чьэцуст0ш3ьц0ч3",
+    "email": """onetwo+simullteniesly.twentyfive-seventyday_samountqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm
+    qwertyuiopasdfg@gmail.com""",
+    "phone": """+123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012
+    3456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567
+    8901234567890123456789012345678901234""",
+    "instagram": """Пейзик!([#}{2@'%"/|^34*.,`~""",
+    "telegram": "ауцтсту.92ьх3ь-!смзц@"
+}
+
+VALID_NEW_PASSWORD = 't1T!k@cK%'
+INVALID_NEW_PASSWORD = ''

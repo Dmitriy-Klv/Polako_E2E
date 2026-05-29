430628e9e3f7a1f9f84fc92917538d5a8b44daeb
Viktoria <boychenkovika1985@gmail.com>
2026-05-29 18:36:01 +0200

    eat(auth): Enhance authentication flows and add validation tests
    - Refactor forgot password and registration tests.
    - Add new test cases for empty email and invalid password validation feedback.
    - Update related page objects and constants.

--- TP_Polako_E2E/pages/auth/registration_page.py
+++ TP_Polako_E2E/pages/auth/registration_page.py
@@ -1,53 +1,38 @@
-from TP_Polako_E2E.utils.constants import (
-    COMPANY_REGISTRATION_SUCCESS_MESSAGE,
-    COOL_BTN_TEXT,
-)
+from playwright.sync_api import expect
 
 from TP_Polako_E2E.base.base_page import BasePage
 from TP_Polako_E2E.pages.auth.login_page import LOGIN_FORM
-from TP_Polako_E2E.utils.constants import (
-    COMPANY_REGISTRATION_SUCCESS_MESSAGE,
-    COOL_BTN_TEXT,
-)
 
 NO_ACCOUNT_BTN = "header div.absolute > form + button"
 ORGANIZER_TAB_BTN = "div.absolute.right-0 div.border-b button:nth-child(2)"
-
 EMAIL_INPUT = 'input[name="email"]'
 PASSWORD_INPUT = 'input[name="password"]'
 FIRST_NAME_INPUT = 'input[name="first_name"]'
 NEXT_BTN = "header div.absolute form button.btn-accent"
-
 COMPANY_NAME_INPUT = 'input[name="company_name"]'
-COMPANY_NAME_ERROR = (
-    'header div.absolute form div:has(> input[name="company_name"]) span'
-)
+COMPANY_NAME_ERROR = 'header div.absolute form div:has(> input[name="company_name"]) span'
 REGISTER_BTN = 'header div.absolute form button[type="submit"]'
-
-CAPCHA_TOKEN = "aHEqAAAAAEvjOmDiC4CVVh16pgaXcFHpQAgt"
-
-RECAPTCHA_VERIFY_BTN = "#recaptcha-verify-button"
-
 SUCCESS_MESSAGE = "div.absolute.right-0 p.text-center.text-2xl"
-COOL_BTN = "div.absolute.right-0 button.btn-accent"
-COMPANY_REGISTRATION_INFORM_MESSAGE = (
-    "div.absolute.right-0 div.flex-col p.mt-3.text-base"
-)
+COMPANY_REGISTRATION_INFORM_MESSAGE = "div.absolute.right-0 div.flex-col p.mt-3.text-base"
 COMPANY_REGISTRATION_ERROR_MESSAGE = "div.absolute.right-0 > p"
+PASSWORD_ERROR_MESSAGE = 'input[name="password"] + text-sm text-red-500'
 
 
 class RegistrationPage(BasePage):
 
-    def go_to_organizer_registration(self):
+    def for_organization_to_click(self):
         self.click(NO_ACCOUNT_BTN)
         self.page.locator(LOGIN_FORM).wait_for(state="visible", timeout=5000)
         self.click(ORGANIZER_TAB_BTN)
 
-    def fill_first_step_form(self, email: str, password: str, first_name: str):
+    def fill_first_step_form(
+        self, email: str, password: str, first_name: str, submit_form: bool = True
+    ):
         self.fill(EMAIL_INPUT, email)
         self.fill(PASSWORD_INPUT, password)
         self.fill(FIRST_NAME_INPUT, first_name)
-        self.click(NEXT_BTN)
+        if submit_form:
+            self.click(NEXT_BTN)
 
     def fill_company_name(self, company_name: str):
         self.fill(COMPANY_NAME_INPUT, company_name)
@@ -62,12 +47,15 @@ class RegistrationPage(BasePage):
         self.verify_element_is_visible(
             SUCCESS_MESSAGE, COMPANY_REGISTRATION_ERROR_MESSAGE, timeout=10000
         )
-        self.verify_element_is_visible(COOL_BTN, COOL_BTN_TEXT)
-
-    def click_cool_button(self):
-        self.click(COOL_BTN)
 
     def verify_failure_inform_message_company_registration(self):
         self.verify_element_is_visible(
             COMPANY_REGISTRATION_INFORM_MESSAGE, COMPANY_REGISTRATION_ERROR_MESSAGE
         )
+
+    def verify_next_button_is_disabled(self):
+        self.page.locator(NEXT_BTN)
+
+    def verify_password_error_is_not_visible(self):
+        locator = self.page.locator(PASSWORD_ERROR_MESSAGE)
+        expect(locator).not_to_be_visible(timeout=5000)
--- TP_Polako_E2E/tests/test_registration.py
+++ TP_Polako_E2E/tests/test_registration.py
@@ -7,53 +7,69 @@ from TP_Polako_E2E.utils.constants import (
     TEST_NAME,
     VALID_COMPANY_NAME,
     VALID_TEST_PASSWORD,
-    generate_random_email,
-    generate_random_user_name,
-    generate_random_company_name,
-    generate_random_password,
+    VALID_RANDOM_USERS_DATA,
+    INVALID_PASSWORD,
 )
 
 from TP_Polako_E2E.base.base_test import BaseTest
-from TP_Polako_E2E.pages.auth.registration_page import (
-    REGISTER_BTN,
-    COMPANY_REGISTRATION_INFORM_MESSAGE,
-)
+from TP_Polako_E2E.pages.auth.registration_page import REGISTER_BTN
 
 
 class TestRegistration(BaseTest):
 
-    @pytest.mark.skip(reason="Blocked by CAPTCHA limitation")
+    @pytest.mark.xfail(reason="Blocked by CAPTCHA limitation")
     def test_positive_manager_registration(self):
-        random_email = generate_random_email()
-        random_user_name = generate_random_user_name()
-        random_company_name = generate_random_company_name()
-        random_password = generate_random_password()
 
         self.login_page.open_login_modal()
-        self.registration_page.go_to_organizer_registration()
-        self.registration_page.fill_first_step_form(random_email, random_password, random_user_name)
-        self.registration_page.fill_company_name(random_company_name)
+        self.registration_page.for_organization_to_click()
+        self.registration_page.fill_first_step_form(
+            VALID_RANDOM_USERS_DATA[0],
+            VALID_RANDOM_USERS_DATA[3],
+            VALID_RANDOM_USERS_DATA[1],
+        )
+        self.registration_page.fill_company_name(VALID_RANDOM_USERS_DATA[2])
         self.registration_page.click_register()
         self.registration_page.verify_success_registration()
 
-
     def test_invalid_company_name_registration(self):
         self.login_page.open_login_modal()
-        self.registration_page.go_to_organizer_registration()
-        self.registration_page.fill_first_step_form(TEST_EMAIL, VALID_TEST_PASSWORD, TEST_NAME)
+        self.registration_page.for_organization_to_click()
+        self.registration_page.fill_first_step_form(
+            VALID_RANDOM_USERS_DATA[0],
+            VALID_RANDOM_USERS_DATA[3],
+            VALID_RANDOM_USERS_DATA[1],
+        )
         self.registration_page.fill_company_name(LONG_COMPANY_NAME)
-        self.registration_page.verify_company_name_error_visible(EXPECTED_ERROR_TEXT_COMPANY_NAME)
+        self.registration_page.verify_company_name_error_visible(
+            EXPECTED_ERROR_TEXT_COMPANY_NAME
+        )
 
         assert self.page.locator(REGISTER_BTN).is_disabled()
 
-
-    @pytest.mark.skip(reason="Blocked by CAPTCHA limitation")
+    @pytest.mark.xfail(reason="Blocked by CAPTCHA limitation")
     def test_email_has_already_been_registered(self):
         self.login_page.open_login_modal()
-        self.registration_page.go_to_organizer_registration()
-        self.registration_page.fill_first_step_form(TEST_EMAIL, VALID_TEST_PASSWORD, TEST_NAME)
+        self.registration_page.for_organization_to_click()
+        self.registration_page.fill_first_step_form(
+            TEST_EMAIL, VALID_TEST_PASSWORD, TEST_NAME
+        )
         self.registration_page.fill_company_name(VALID_COMPANY_NAME)
         self.registration_page.click_register()
         self.registration_page.verify_failure_inform_message_company_registration()
 
-        assert self.page.locator(COMPANY_REGISTRATION_INFORM_MESSAGE)
+    def test_company_registration_with_empty_fields(self):
+        self.login_page.open_login_modal()
+        self.registration_page.for_organization_to_click()
+        self.registration_page.verify_next_button_is_disabled()
+        self.page.pause()
+
+    def test_company_registration_with_password_error_not_displayed(self):
+        self.login_page.open_login_modal()
+        self.registration_page.for_organization_to_click()
+        self.registration_page.fill_first_step_form(
+            VALID_RANDOM_USERS_DATA[0],
+            INVALID_PASSWORD[5],
+            VALID_RANDOM_USERS_DATA[1],
+            submit_form=False,
+        )
+        self.registration_page.verify_password_error_is_not_visible()
